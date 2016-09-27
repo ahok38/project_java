bc9c345bf0e465b089e6cadab51431fd99daa0fa
Jonas Tschammer <Jonas.Tschammer@bio.uni-giessen.de>
2016-09-27 15:28:50 +0200

    Klassen Room und Rooms erstellt. Erlauben es, die Datei rooms.txt einzulesen und in Objekte der Klasse Room zu verwandeln, sowie durch das Verlies zu navigieren.

--- src/gameMechanics/Room.java
+++ src/gameMechanics/Room.java
@@ -1,123 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package gameMechanics;
-
 /**
- *
- * @author Martins
+ * Modelliert einen Raum in einem Verlies.
+ * Created by jonas on 27/09/16.
  */
 public class Room {
-    String Name = "";
-    boolean start = false;
-    boolean wayOut = false;
-    String roomNorth = "none";
-    String roomSouth = "none";
-    String roomEast = "none";
-    String roomWest = "none";
-    String description = "none";
-    String content = "none";
-    NPC[] npcInsideRoom;
-    Player[] playersInRoom;
-    Item[] itemsInsideRoom;
-    FixItem[] itemsFixInRoom;
-    Door[] doorsInsideRoom;
-    FixItem[] fixItemsInRoom;
+    private String name;
+    private String nord;
+    private String ost;
+    private String sued;
+    private String west;
+    private String description;
+    private String content;
 
-    void events () {
-        // TODO
+    public Room(String name) {
+        this.name = name;
     }
 
-    public Room(String ininitalName, boolean ininitalStart, boolean ininitalWayOut, String ininitalRoomNorth, String ininitalRoomSouth, String ininitalRoomEast, String ininitalRoomWest,
-            String ininitalDescription, String ininitalContent, NPC[] ininitalNpcInsideRoom, Player[] ininitalPlayersInRoom, Item[] ininitalItemsInsideRoom,
-            FixItem[] ininitalItemsFixInRoom, Door[] ininitalDoorsInsideRoom, FixItem[] ininitalFixItemsInRoom) {
-        Name = ininitalName;
-        start = ininitalStart;
-        wayOut = ininitalWayOut;
-        roomNorth = ininitalRoomNorth;
-        roomSouth = ininitalRoomSouth;
-        roomEast = ininitalRoomEast;
-        roomWest = ininitalRoomWest;
-        description = ininitalDescription;
-        content = ininitalContent;
-        npcInsideRoom = ininitalNpcInsideRoom;
-        playersInRoom = ininitalPlayersInRoom;
-        itemsInsideRoom = ininitalItemsInsideRoom;
-        itemsFixInRoom = ininitalItemsFixInRoom;
-        doorsInsideRoom = ininitalDoorsInsideRoom;
-        fixItemsInRoom = ininitalFixItemsInRoom;
+
+    public Room(String name, String nord, String ost, String sued, String west, String description, String content) {
+        this.name = name;
+        this.nord = nord;
+        this.ost = ost;
+        this.sued = sued;
+        this.west = west;
+        this.description = description;
+        this.content = content;
     }
-    
-    public Room(){
-        
+
+    public String getName() {
+        return name;
     }
-    
-    public String getName(){
-        return Name;
+
+
+    public String getNord() {
+        return nord;
     }
-    public void setName(String nameInput){
-        Name = nameInput;
+
+
+    public String getOst() {
+        return ost;
     }
-    public boolean getStart(){
-        return start;
+
+
+    public String getSued() {
+        return sued;
     }
-    public void setStart(boolean startInput){
-        start = startInput;
+
+
+    public String getWest() {
+        return west;
     }
-     public boolean getWayOut(){
-        return wayOut;
-    }
-    public void setWayOut(boolean wayOutInput){
-        wayOut = wayOutInput;
-    }
-    public String getRoomNorth(){
-        return roomNorth;
-    }
-    public void setRoomNorth(String roomNorthInput){
-        roomNorth = roomNorthInput;
-    }
-    public String getRoomSouth(){
-        return roomSouth;
-    }
-    public void setRoomSouth(String roomSouthInput){
-        roomSouth = roomSouthInput;
-    }
-    public String getRoomEast(){
-        return roomEast;
-    }
-    public void setRoomEast(String roomEastInput){
-        roomEast = roomEastInput;
-    }
-    public String getRoomWest(){
-        return roomWest;
-    }
-    public void setRoomWest(String roomWestInput){
-        roomWest = roomWestInput;
-    }
-    public String getDescription(){
+
+
+    public String getDescription() {
         return description;
     }
-    public void setDescription(String descriptionInput){
-        description = descriptionInput;
-    }
-    public String getContent(){
+
+
+    public String getContent() {
         return content;
-    }
-    public void setContent(String contentInput){
-        content = contentInput;
-    }
-    public void populateRoom() {
-        // TODO
-    }
-
-    public void createInventory() {
-        // TODO
-    }
-
-    public void createEncounter() {
-        // TODO
     }
 
 }
