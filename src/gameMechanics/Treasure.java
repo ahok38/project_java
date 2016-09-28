72a540d41c489b804432479735065b80fbe2a739
Andreas Hoek <andreas.hoek@gmx.de>
2016-09-28 18:50:16 +0200

    Fabians Dateien eingefuegt.

--- src/gameMechanics/Treasure.java
+++ src/gameMechanics/Treasure.java
@@ -3,12 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gameMechanics;
+package dnd.gameMechanics;
 
 /**
- *
- * @author Martins
+ * This class implements features of treasures.
+ * Treasures implement methods from Item
+ * @author Fabian Billenkamp
  */
-public class Treasure extends Item{
-    String Name;
+public class Treasure implements Item{
+    private String name;
+    private String description;
+
+    /**
+     * Constructor for a treasure with name, value and description
+     * @param name String: the name of the treasure
+     * @param description String: description for the treasure
+     */
+    public Treasure(String name,String description){
+        this.setName(name);
+        this.setDescription(description);
+    }
+
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        this.description=description;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name=name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
 }
