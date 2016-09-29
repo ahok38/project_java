637fa073ea41892cfc1aa9c8ca2ac7a79ef30754
Patrick Barth <Patrick.Barth@bio.uni-giessen.de>
2016-09-29 12:17:59 +0200

    Creatures und Weapons werden in HashMaps anstatt Arrays ausgegeben. Creatures werden direkt mit Weapon-Objekten erstellt

--- src/data/Parser.java
+++ src/data/Parser.java
@@ -1,13 +1,13 @@
 package data;
 
 import gameMechanics.Creature;
-import gameMechanics.NPC;
 import gameMechanics.Weapon;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 /**
  * Created by Patrick on 15.09.2016.
@@ -16,11 +16,10 @@ public class Parser {
 
     private Parser(){}
 
-
-    private static Creature[] parseCreatures (String file)throws IOException{
+    private static HashMap<String,Creature> parseCreatures (String file)throws IOException{
         BufferedReader br=readFile(file);
 
-        Creature[] allCreatures=new Creature[countLines(br)/9+1];
+        HashMap<String,Creature> allCreatures=new HashMap<>();
 
         String name="";
         String species="";
@@ -29,13 +28,9 @@ public class Parser {
         int hp=0;
         int baseDamage=0;
         int armor=0;
-        Weapon weapon=new Weapon();
-
-        int arrayCount=0;
+        Weapon weapon=new Weapon("",0,"");
 
         String line;
-
-        br=readFile(file);
 
         while((line=br.readLine())!=null){
 
@@ -54,23 +49,55 @@ public class Parser {
             } else if(line.matches("Armor: .*")){
                 armor=Integer.valueOf(line.substring(7));
             } else if(line.matches("Weapon: .*")){
-                weapon=new Weapon();
+
+               weapon=collectWeapons().get(line.substring(8));
             } else if(line.matches("")){
-                allCreatures[arrayCount]=new Creature(name,species,description,xp,hp,baseDamage,armor,weapon);
-                arrayCount++;
+                allCreatures.put(name,new Creature(name,species,description,xp,hp,baseDamage,armor,weapon));
             }
         }
-        allCreatures[arrayCount]=new Creature(name,species,description,xp,hp,baseDamage,armor,weapon);
+        allCreatures.put(name,new Creature(name,species,description,xp,hp,baseDamage,armor,weapon));
 
         return allCreatures;
     }
 
-    private static int countLines(BufferedReader br)throws IOException{
-        int lineNumber=0;
-        while(br.readLine() != null){
-            lineNumber++;
+    private static HashMap<String,Weapon> parseWeapons (String file)throws IOException{
+        BufferedReader br=readFile(file);
+
+        HashMap<String,Weapon> allWeapons=new HashMap<>();
+
+        String name="";
+        int force=0;
+        String description="";
+
+
+        String line;
+
+        while((line=br.readLine())!=null){
+
+            if(line.matches("Name: .*")){
+                name=line.substring(6);
+            } else if(line.matches("Force: .*")){
+                force=Integer.valueOf(line.substring(7));
+            } else if(line.matches("Description: .*")){
+                description=line.substring(13);
+
+            } else if(line.matches("")){
+                allWeapons.put(name,new Weapon(name,force,description));
+            }
         }
-        return lineNumber;
+        allWeapons.put(name,new Weapon(name,force,description));
+
+        return allWeapons;
+    }
+
+    /**
+     * Returns an array that contains all weapons saved in weapons.txt
+     * @returnArray  with all weapons
+     * @throws IOException
+     */
+
+    public static HashMap<String, Weapon> collectWeapons()throws IOException{
+        return parseWeapons("src/data/weapons.txt");
     }
 
     /**
@@ -78,9 +105,22 @@ public class Parser {
      * @return Array with all creatures
      * @throws IOException
      */
-    public static Creature[] collectCreatures()throws IOException{
+
+    public static HashMap<String, Creature> collectCreatures()throws IOException{
         return parseCreatures("src/data/creatures.txt");
     }
+
+
+
+
+
+    /*private static int countLines(BufferedReader br)throws IOException{
+        int lineNumber=0;
+        while(br.readLine() != null){
+            lineNumber++;
+        }
+        return lineNumber;
+    }*/
 
     private static BufferedReader readFile(String file) throws IOException{
         FileInputStream fis = new FileInputStream(file);
--- src/gameMechanics/Weapon.java
+++ src/gameMechanics/Weapon.java
@@ -10,5 +10,38 @@ package gameMechanics;
  * @author Martins
  */
 public class Weapon extends Item{
-    
+
+    String name;
+    int force;
+    String description;
+
+    public Weapon(String name, int force, String description) {
+        this.setName(name);
+        this.setForce(force);
+        this.setDescription(description);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
